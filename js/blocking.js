829c3c6171e3e859720513193ec518162efc5111
Jared Sulzdorf <j.sulzdorf@gmail.com>
2016-07-08 11:28:20 -0700

    proof of concept text replace function working

--- js/blocking.js
+++ js/blocking.js
@@ -1,1 +1,22 @@
-document.title = 'new string';
+function replace() {
+	tags = document.getElementsByTagName('*');
+
+	for ( var i = 0; i < tags.length; i++) {
+		var element = tags[i];
+
+		for ( var j = 0; j < element.childNodes.length; j++) {
+			var node = element.childNodes[j];
+
+			if ( node.nodeType === 3 ) {
+				var text = node.nodeValue;
+				var newText = text.replace( /Monitor/gi, 'stuff' );
+
+				if ( newText !== text ) {
+					element.replaceChild( document.createTextNode(newText), node );
+				}
+			}
+		}
+	}
+}
+
+replace();
